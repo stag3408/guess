813d926748b8c817cf098a8215c4c9b8cd331086
stag3408 <stag3408@gmail.com>
2019-09-01 20:19:26 +0100

    final version

--- guess.py
+++ guess.py
@@ -1,8 +1,13 @@
 import random
+start = input('輸入起始值: ')
+end = input('輸入結束值: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
-
+r = random.randint(start, end)
+n = 0
 while True:
+	n = n+1
 	num = input('請輸入數字: ')
 	num = int(num)
 	if num > r:
@@ -11,4 +16,6 @@ while True:
 		print('比數字小')
 	elif num == r:
 		print('你猜對了!')
+		print('你共猜了', n, '次')
 		break
+	print('你共猜了', n, '次')
